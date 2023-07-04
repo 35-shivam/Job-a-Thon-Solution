574e65cdd966c9ca4aebe3eac27f29506d226c5d
shivam sharma <shivamsharma1642003@gmail.com>
2023-07-04 14:46:43 +0530

    adding addition function

--- Main.java
+++ Main.java
@@ -2,4 +2,7 @@ class Main{
     public static void main(String[] args) {
         System.out.println("initial commit");
     }
+    public static int add(int a, int b){
+        return a|b;
+    }
 }
