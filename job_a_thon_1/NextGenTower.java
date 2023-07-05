fbae9af327e26fc3568f29533f3a7e76919d7908
shivam sharma <shivamsharma1642003@gmail.com>
2023-07-05 09:52:57 +0530

    updating NextGenTower Class

--- job_a_thon_1/NextGenTower.java
+++ job_a_thon_1/NextGenTower.java
@@ -1,10 +1,5 @@
 package job_a_thon_1;
 
 class NextGenTower{
-    public static void main(String[] args) {
-        System.out.println("In process");
-    }
-    public static int func(int a){
-        return a^2;
-    }
+    
 }
